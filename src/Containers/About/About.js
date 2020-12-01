3e0378f11f55d957ff2e623b375fb487d07e0bcc
Tenkorang Daniel <42743956+DocTea004@users.noreply.github.com>
2020-11-30 16:36:31 -0800

    Update About.js

--- src/Containers/About/About.js
+++ src/Containers/About/About.js
@@ -17,10 +17,10 @@ function About() {
             <div 
             className="about-imagecont">
                 <Card.Img
-                class="img-fluid w-0.5 h-0.5 img-responsive center-block d-block mx-auto"
+                class="img-fluid w-0.4 h-0.4 img-responsive center-block d-block mx-auto"
                 src={require('../../Image/sefa.jpg')}
                 style={{
-                    width:'500px',
+                    width:'350px',
                     boxShadow: '0px 2px 3px rgba(51, 30, 30, 0.13) ,1px 2px 2px rgba(44, 14, 14, 0.1) , -1px -2px 2px rgba(0,0,0,.05) ',
                     boxShadow: '10px',
                 }}
@@ -29,12 +29,9 @@ function About() {
 
             <div className="about-description">
                 <p>
-                I’m <span>Mr.Augustine Sefah </span> 
+                I’m <span>Mr.Augustine Sefa </span> 
                 the owner and managing director of 
                 <span> 24/7 SOUND AND SECURED SECURITY SERVICES LTD </span> 
-                I'm 38 years of age, born and raised at the Kwahu-Afram Plains. 
-                I attended my basic school at Tease Presby J.H.S and my Secondary Education
-                at Mpraeso Secondary School between 1997 and 1999 December.
                 I came into Security Installation and Sales Job in 2007, trained at AZALES ENT.
                 Until I completed my training and education, then I moved on to establish 24/7 SOUND AND SECURED SECURITY SERVICES LTD. 
                 24/7 SOUND AND SECURED SECURITY SERVICES LTD has rendered a lot of excellent services to industries,
